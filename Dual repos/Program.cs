5ed9331b42e39c5fe7f51e0825e462deeb9da2e3
pixel-pick <chegoiahz@yahoo.com>
2025-04-20 13:36:15 +0300

    вычисление целочисленных координат внутри круга

--- Dual repos/Program.cs
+++ Dual repos/Program.cs
@@ -1,3 +1,32 @@
-﻿string Second = "Полностью пустой сайт";
-String First = "https://gzmland.ru/";
-Console.Write(First + " " + Second);
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите радиус круга (R > 0): ");
+        double R = double.Parse(Console.ReadLine());
+
+        if (R <= 0)
+        {
+            Console.WriteLine("Радиус должен быть больше нуля.");
+            return;
+        }
+
+        int count = 0;
+        int radiusInt = (int)Math.Floor(R);
+
+        for (int x = -radiusInt; x <= radiusInt; x++)
+        {
+            for (int y = -radiusInt; y <= radiusInt; y++)
+            {
+                if (x * x + y * y <= R * R)
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine($"Количество точек с целочисленными координатами: {count}");
+    }
+}
